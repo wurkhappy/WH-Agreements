ddacb0f53afe246618077f4e04e2a80eda11c579
Matt Parker <mdparker89@gmail.com>
2013-10-29 22:22:35 -0400

    add userID to clauses

--- models/clause.go
+++ models/clause.go
@@ -1,6 +1,7 @@
 package models
 
 type Clause struct {
-	ID   int    `json:"id"`
-	Text string `json:"text"`
+	ID     int    `json:"id"`
+	Text   string `json:"text"`
+	UserID string `json:"userID"`
 }
