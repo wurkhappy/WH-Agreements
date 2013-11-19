4c1c7be3c68fb68dfbd197a565a6645ded89bb92
Matt Parker <mdparker89@gmail.com>
2013-11-19 13:57:23 -0500

    add required field to payment struct

--- models/payment.go
+++ models/payment.go
@@ -8,11 +8,12 @@ type Payment struct {
 	//these are attributes that the service manages the data for
 	//eventually this stuff could be broken out into its own service but overkill right now
 	ID            string       `json:"id" bson:"_id"`
-	Amount        int          `json:"amount"`//cents not dollars
+	Amount        int          `json:"amount"` //cents not dollars
 	ScopeItems    []*ScopeItem `json:"scopeItems"`
 	Title         string       `json:"title"`
 	CurrentStatus *Status      `json:"currentStatus" bson:",omitempty"`
 	DateExpected  time.Time    `json:"dateExpected"`
+	Required      bool         `json:"required"`
 
 	//we won't store this but we need this data to delegate to the payment service
 	PaymentMethodID    string `json:"paymentMethodID" bson:"-"`
