d0836a6d7636131c5d9788a6f25d94cf04adc560
Matt Parker <mdparker89@gmail.com>
2014-01-16 21:49:02 -0500

    escape html for proposedServices

--- handlers/agreements.go
+++ handlers/agreements.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/wurkhappy/WH-Agreements/models"
+	"html/template"
 	"net/http"
 )
 
@@ -16,6 +17,7 @@ func CreateAgreement(params map[string]interface{}, body []byte) ([]byte, error,
 	}
 	agreement.SetDraftCreatorID()
 	agreement.WorkItems.AddIDs()
+	agreement.ProposedServices = template.HTMLEscapeString(agreement.ProposedServices)
 	err = agreement.Save()
 	if err != nil {
 		return nil, fmt.Errorf("%s %s", "Error saving: ", err.Error()), http.StatusBadRequest
